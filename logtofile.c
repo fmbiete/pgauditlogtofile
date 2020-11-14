a143c41517fcc6700d398f9fd8ccdb92010d930b
Francisco Miguel Biete Banon <fbiete@gmail.com>
2020-11-14 11:00:04 +0000

    Fix: use current timestamp for audit line instead of calculated time for SYSLOG

--- logtofile.c
+++ logtofile.c
@@ -284,12 +284,8 @@ static void fmtLogLine(StringInfo buf, ErrorData *edata) {
   /*
    * timestamp with milliseconds
    *
-   * Check if the timestamp is already calculated for the syslog message,
-   * and use it if so.  Otherwise, get the current timestamp.  This is done
-   * to put same timestamp in both syslog and csvlog messages.
    */
-  if (formatted_log_time[0] == '\0')
-    setup_formatted_log_time();
+  setup_formatted_log_time();
 
   appendStringInfoString(buf, formatted_log_time);
   appendStringInfoChar(buf, ',');
