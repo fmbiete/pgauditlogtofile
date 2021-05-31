f55f7fcfeadaedb521e681c06e569cc53b9e7910
Francisco Miguel Biete Banon <fbiete@gmail.com>
2021-05-31 09:19:01 +0100

    Fix: add missing update path

---
+++ pgauditlogtofile--1.0--1.2.sql
@@ -0,0 +1,5 @@
+/* pgauditlogtofile/pgauditlogtofile--1.0--1.2.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION pgauditlogtofile UPDATE TO '1.2'" to load this file. \quit
+
