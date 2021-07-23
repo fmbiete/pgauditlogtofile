f6c5d78d3fa9ccb2bcac4c970f6a96bc9e00523c
Francisco Miguel Biete Banon <fbiete@gmail.com>
2021-07-23 14:59:38 +0100

    Fix: #9 - segfault on SIGHUP

---
+++ pgauditlogtofile--1.2--1.3.sql
@@ -0,0 +1,4 @@
+/* pgauditlogtofile/pgauditlogtofile--1.2--1.3.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION pgauditlogtofile UPDATE TO '1.3'" to load this file. \quit
