d817883d0b8bbc0573540fa034b90202134ca6ec
Francisco Miguel Biete Banon <fbiete@gmail.com>
2021-09-19 10:14:09 +0100

    Change: prepare bump to v1.4

---
+++ pgauditlogtofile--1.3--1.4.sql
@@ -0,0 +1,4 @@
+/* pgauditlogtofile/pgauditlogtofile--1.3--1.4.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION pgauditlogtofile UPDATE TO '1.4'" to load this file. \quit
