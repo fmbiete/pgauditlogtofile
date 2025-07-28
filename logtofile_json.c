5b0d4ea91e8fce86e3a1981afd4bb51d4183717e
Francisco Miguel Biete Banon <fbiete@gmail.com>
2025-07-28 18:16:55 +0100

    Change: refactored to accept format like override

--- logtofile_json.c
+++ logtofile_json.c
@@ -21,11 +21,14 @@
 #include <utils/json.h>
 #include <utils/ps_status.h>
 
+#include <stdarg.h>
+
 /* forward declaration private functions */
 
 static void pgauditlogtofile_pgaudit2json(StringInfo buf, char *message);
 static void pgauditlogtofile_append_json_key_value(StringInfo buf, const char *key, const char *value);
-static void pgauditlogtofile_append_json_key(StringInfo buf, const char *key);
+static void pgauditlogtofile_append_json_key_fmt(StringInfo buf, const char *key, const char *fmt, ...)
+    __attribute__((format(gnu_printf, 3, 4)));
 
 /**
  * @brief Creates a json audit record
@@ -56,8 +59,7 @@ void PgAuditLogToFile_json_audit(StringInfo buf, const ErrorData *edata, int exc
     pgauditlogtofile_append_json_key_value(buf, "db.name", MyProcPort->database_name);
 
   /* Process id  */
-  pgauditlogtofile_append_json_key(buf, "custom.process_id");
-  appendStringInfo(buf, "\"%d\"", MyProcPid);
+  pgauditlogtofile_append_json_key_fmt(buf, "custom.process_id", "%d", MyProcPid);
 
   /* Remote host and port */
   if (MyProcPort && MyProcPort->remote_host)
@@ -68,8 +70,7 @@ void PgAuditLogToFile_json_audit(StringInfo buf, const ErrorData *edata, int exc
   }
 
   /* session id - hex representation of start time . session process id */
-  pgauditlogtofile_append_json_key(buf, "custom.session_id");
-  appendStringInfo(buf, "\"%lx.%x\"", (long)MyStartTime, MyProcPid);
+  pgauditlogtofile_append_json_key_fmt(buf, "custom.session_id", "%lx.%x", (long)MyStartTime, MyProcPid);
 
   /* PS display */
   if (MyProcPort)
@@ -93,20 +94,17 @@ void PgAuditLogToFile_json_audit(StringInfo buf, const ErrorData *edata, int exc
 #if (PG_VERSION_NUM >= 170000)
   if (MyProc != NULL && MyProc->vxid.procNumber != INVALID_PROC_NUMBER)
   {
-    pgauditlogtofile_append_json_key(buf, "custom.virtual_transaction_id");
-    appendStringInfo(buf, "\"%d/%u\"", MyProc->vxid.procNumber, MyProc->vxid.lxid);
+    pgauditlogtofile_append_json_key_fmt(buf, "custom.virtual_transaction_id", "%d/%u", MyProc->vxid.procNumber, MyProc->vxid.lxid);
   }
 #else
   if (MyProc != NULL && MyProc->backendId != InvalidBackendId)
   {
-    pgauditlogtofile_append_json_key(buf, "custom.virtual_transaction_id");
-    appendStringInfo(buf, "\"%d/%u\"", MyProc->backendId, MyProc->lxid);
+    pgauditlogtofile_append_json_key_fmt(buf, "custom.virtual_transaction_id", "%d/%u", MyProc->backendId, MyProc->lxid);
   }
 #endif
 
   /* Transaction id */
-  pgauditlogtofile_append_json_key(buf, "custom.virtual_transaction_id");
-  appendStringInfo(buf, "\"%u\"", GetTopTransactionIdIfAny());
+  pgauditlogtofile_append_json_key_fmt(buf, "custom.virtual_transaction_id", "%u", GetTopTransactionIdIfAny());
 
   /* SQL state code */
   pgauditlogtofile_append_json_key_value(buf, "custom.state_code", unpack_sql_state(edata->sqlerrcode));
@@ -134,8 +132,7 @@ void PgAuditLogToFile_json_audit(StringInfo buf, const ErrorData *edata, int exc
   /* if printed internal query, print internal pos too */
   if (edata->internalpos > 0 && edata->internalquery != NULL)
   {
-    pgauditlogtofile_append_json_key(buf, "custom.internal_query_pos");
-    appendStringInfo(buf, "\"%d\"", edata->internalpos);
+    pgauditlogtofile_append_json_key_fmt(buf, "custom.internal_query_pos", "%d", edata->internalpos);
   }
 
   /* errcontext */
@@ -148,8 +145,7 @@ void PgAuditLogToFile_json_audit(StringInfo buf, const ErrorData *edata, int exc
     pgauditlogtofile_append_json_key_value(buf, "custom.debug_query", debug_query_string);
     if (edata->cursorpos > 0)
     {
-      pgauditlogtofile_append_json_key(buf, "custom.cursor_pos");
-      appendStringInfo(buf, "\"%d\"", edata->cursorpos);
+      pgauditlogtofile_append_json_key_fmt(buf, "custom.cursor_pos", "%d", edata->cursorpos);
     }
   }
 
@@ -211,11 +207,10 @@ pgauditlogtofile_pgaudit2json(StringInfo buf, char *line)
 }
 
 /*
- * Copied from src/backend/utils/error/jsonlog.c appendJSONKeyValue (private)
+ * Derived from src/backend/utils/error/jsonlog.c appendJSONKeyValue (private)
  *
  * Append to a StringInfo a comma followed by a JSON key and a value.
- * The key is always escaped.  The value can be escaped optionally, that
- * is dependent on the data type of the key.
+ * The key is always escaped.  The value is always escaped.
  */
 static void
 pgauditlogtofile_append_json_key_value(StringInfo buf, const char *key, const char *value)
@@ -223,24 +218,38 @@ pgauditlogtofile_append_json_key_value(StringInfo buf, const char *key, const ch
   if (value == NULL)
     return;
 
-  pgauditlogtofile_append_json_key(buf, key);
-
-  escape_json(buf, value);
-}
-
-/*
- * Copied from src/backend/utils/error/jsonlog.c appendJSONKeyValue (private)
- *
- * Append to a StringInfo a comma followed by a JSON key and a value.
- * The key is always escaped.  The value can be escaped optionally, that
- * is dependent on the data type of the key.
- */
-static void
-pgauditlogtofile_append_json_key(StringInfo buf, const char *key)
-{
-  Assert(key != NULL);
+  if (key == NULL)
+    return;
 
   appendStringInfoChar(buf, ',');
   escape_json(buf, key);
   appendStringInfoChar(buf, ':');
+  escape_json(buf, value);
+}
+
+/**
+ * @brief Append to a StringInfo a json key+value pair with quotes.
+ * @param buf where to write
+ * @param key json attribute key
+ * @param fmt sprintf like formatting string
+ * @param any format parameters
+ */
+static void
+pgauditlogtofile_append_json_key_fmt(StringInfo buf, const char *key, const char *fmt, ...)
+{
+  StringInfoData formatted_value;
+  va_list args;
+
+  if (fmt == NULL)
+    return;
+
+  initStringInfo(&formatted_value);
+
+  va_start(args, fmt);
+  appendStringInfoVA(&formatted_value, fmt, args);
+  va_end(args);
+
+  pgauditlogtofile_append_json_key_value(buf, key, formatted_value.data);
+
+  pfree(formatted_value.data);
 }
